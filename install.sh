ca5e8a73ebfb4f53f4df34c7728bb5870630b8ac
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2021-09-03 21:10:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget "https://github.com/MikMuellerDev/SchoolNotes-vs/releases/download/v1.0.0/schoolnotes-vs-1.0.0.vsix"
+wget "https://github.com/MikMuellerDev/SchoolNotes-vs/raw/v1.0.0/schoolnotes-vs-1.0.0.vsix"
 code --install-extension schoolnotes-vs-1.0.0.vsix
 rm schoolnotes-vs-1.0.0.vsix
 echo "The SchoolNotes extension installer is finished"
