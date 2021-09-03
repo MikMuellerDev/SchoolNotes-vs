478f6b89f9589bb2217349a3dd7da73a76e65aea
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2021-09-03 21:12:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /tmp
 wget "https://github.com/MikMuellerDev/SchoolNotes-vs/raw/v1.0.0/schoolnotes-vs-1.0.0.vsix"
 code --install-extension schoolnotes-vs-1.0.0.vsix
 rm schoolnotes-vs-1.0.0.vsix
